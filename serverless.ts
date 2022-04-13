c833f94b032dd73243fe16bbf67c7095813b5438
sakthi s <velsakthis5301@gmail.com>
2022-04-13 19:20:16 +0530

    first commit

---
+++ serverless.ts
@@ -0,0 +1,64 @@
+import type { AWS } from '@serverless/typescript';
+
+const serverlessConfiguration: AWS = {
+    service: 'serverless',
+    frameworkVersion: '2',
+    custom: {
+        webpack: {
+            webpackConfig: './webpack.config.js',
+            includeModules: true,
+        },
+        bucketName: 'my-certificate-templates-${self:provider.stage}',
+    },
+    // Add the serverless-webpack plugin
+    plugins: ['serverless-webpack'],
+    provider: {
+        name: 'aws',
+        runtime: 'nodejs12.x',
+
+        region: 'us-east-2',
+        profile: 'ServerlessUser',
+
+        apiGateway: {
+            minimumCompressionSize: 1024,
+            binaryMediaTypes: ['*/*'],
+        },
+        environment: {
+            AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+            bucketName: '${self:custom.bucketName}',
+        },
+        iamRoleStatements: [
+            {
+                Effect: 'Allow',
+                Action: 's3:*',
+                Resource: '*',
+            },
+        ],
+    },
+    functions: {
+        createCertificate: {
+            handler: 'src/function/createCertificate/index.handler',
+            events: [
+                {
+                    http: {
+                        method: 'post',
+                        path: 'certificate',
+                    },
+                },
+            ],
+        },
+    },
+
+    resources: {
+        Resources: {
+            templateBucket: {
+                Type: 'AWS::S3::Bucket',
+                Properties: {
+                    BucketName: '${self:custom.bucketName}',
+                },
+            },
+        },
+    },
+};
+
+module.exports = serverlessConfiguration;
